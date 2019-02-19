1355d3ea50a2e91e415366860feff9b511a0731b
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-19 17:10:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,13 +6,22 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path !="/items"
-      resp.write "You requested the items"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
 
+      if @@items.find {|item| item.name == item_name}
+        item_price = @@items.find {|item| item.name == item_name}.price
+        resp.write item_price
+    else
+      resp.write "Item not found"
+      resp.status = 400
+    end
+    else
+      resp.write "Route not found"  
+      resp.status = 404
+    end  
+    
     resp.finish
   end
+  
 end
